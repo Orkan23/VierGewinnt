992c6059ef5d505ad0b388c4dba17acd3f36279f
victorgaenshirt <vgaenshirt@posteo.de>
2023-06-04 23:13:00 +0200

    initial DAOMongoDBImpl file

---
+++ persistence/src/main/scala/de/htwg/se/VierGewinnt/persist/database/mongoDB/DAOMongoDBImpl.scala
@@ -0,0 +1,17 @@
+package de.htwg.se.VierGewinnt.persist.database.mongoDB
+
+import de.htwg.se.VierGewinnt.persist.database.DAOInterface
+
+class DAOMongoDBImpl extends DAOInterface {
+  override def create(): Unit = None
+  // daoPlayground.create()
+
+  override def read(): String = ""
+  // daoPlayground.read()
+
+  override def update(input: String): Unit = None
+  // daoPlayground.update()
+
+  override def delete(): Unit = None
+  // daoPlayground.delete()
+}
