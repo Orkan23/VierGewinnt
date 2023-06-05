6164e128e6aed836e61b3ef98b07b1937b6e1739
victorgaenshirt <vgaenshirt@posteo.de>
2023-06-05 23:17:24 +0200

    start of mondodb implementation

--- persistence/src/main/scala/de/htwg/se/VierGewinnt/persist/database/mongoDB/DAOMongoDBImpl.scala
+++ persistence/src/main/scala/de/htwg/se/VierGewinnt/persist/database/mongoDB/DAOMongoDBImpl.scala
@@ -1,12 +1,20 @@
 package de.htwg.se.VierGewinnt.persist.database.mongoDB
 
 import de.htwg.se.VierGewinnt.persist.database.DAOInterface
+import org.mongodb.scala.*
 
 class DAOMongoDBImpl extends DAOInterface {
+
+  private val databaseUrl: String = "mongodb://localhost:27017"
+
+  val client: Option[MongoClient] = Some(MongoClient(databaseUrl))
+  val db: MongoDatabase = client.getDatabase("vierGewinnt")
+  val collection: MongoCollection[Document] = db.getCollection("game")
   override def create(): Unit = None
   // daoPlayground.create()
 
   override def read(): String = ""
+  val res: MongoCollection[Document] = db.getCollection("test")
   // daoPlayground.read()
 
   override def update(input: String): Unit = None
