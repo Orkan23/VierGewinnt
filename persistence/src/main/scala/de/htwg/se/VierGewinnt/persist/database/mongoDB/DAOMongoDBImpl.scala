ea3765f88852ce14a347982875d2701c0ef9373c
victorgaenshirt <vgaenshirt@posteo.de>
2023-06-07 03:35:11 +0200

    implementation of DAOMongoDBImpl

--- persistence/src/main/scala/de/htwg/se/VierGewinnt/persist/database/mongoDB/DAOMongoDBImpl.scala
+++ persistence/src/main/scala/de/htwg/se/VierGewinnt/persist/database/mongoDB/DAOMongoDBImpl.scala
@@ -1,7 +1,16 @@
 package de.htwg.se.VierGewinnt.persist.database.mongoDB
 
 import de.htwg.se.VierGewinnt.persist.database.DAOInterface
-import org.mongodb.scala.*
+import org.mongodb.scala._
+import org.mongodb.scala.model._
+import play.api.libs.json.JsValue
+import play.api.libs.json.Json
+import org.mongodb.scala.bson.collection.immutable.Document
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Updates._
+import org.mongodb.scala.model.UpdateOptions
+import org.mongodb.scala.model.Projections._
+import java.util.Random
 
 class DAOMongoDBImpl extends DAOInterface {
 
@@ -10,16 +19,24 @@ class DAOMongoDBImpl extends DAOInterface {
   val client: Option[MongoClient] = Some(MongoClient(databaseUrl))
   val db: MongoDatabase = client.getDatabase("vierGewinnt")
   val collection: MongoCollection[Document] = db.getCollection("game")
-  override def create(): Unit = None
-  // daoPlayground.create()
+  override def create(json: String): Int = {
+    val rand: Random = new Random()
+    val id = rand.nextInt(900000)
+  val document: Document = Document("_id" -> id, "game" -> json)
+  val query = collection.insertOne(document)
+  id
+  }
 
-  override def read(): String = ""
-  val res: MongoCollection[Document] = db.getCollection("test")
-  // daoPlayground.read()
+  override def read(): String = {
+    collection.find().printResults()
+  }
 
-  override def update(input: String): Unit = None
-  // daoPlayground.update()
+  override def update(input: String): Unit ={
+  val json: JsValue = Json.parse(input)
+  collection.insertOne(Document(json))
+  }
 
-  override def delete(): Unit = None
-  // daoPlayground.delete()
+  override def delete(id: Int): Unit = {
+    collection.deleteOne(equal("id", id))
+  }
 }
